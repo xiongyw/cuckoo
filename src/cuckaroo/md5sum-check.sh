df1f7e4e43f0eca167ed2a3f34ddcd45276cd104
Yuwu Xiong <5070319@qq.com>
2019-06-28 09:31:37 +0800

    add

---
+++ src/cuckaroo/md5sum-check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./cuda29 -n 671 > /dev/null
+#md5sum SeedA-bufferAB.bin
+#md5sum SeedA-indexesE1.bin
+#md5sum SeedB-bufferA.bin
+#md5sum SeedB-indexesE0.bin
+#md5sum Round0-indexesE1.bin
+#md5sum Round0-indexesE2.bin
+md5sum Round1-indexesE0.bin
